11530843cc3c26af270599053588bf7a4900e322
polinnab <p.tsunenko94@gmail.com>
2022-06-10 10:59:09 +0300

    first commit

--- config/database.js
+++ config/database.js
@@ -2,10 +2,20 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
+    client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'gatsby-blog'),
+        user: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'Postgres74'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not required
+        ssl: false
+      },
+    // client: 'sqlite',
+    // connection: {
+    //   filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+    // },
     useNullAsDefault: true,
   },
 });
